269dd7a8cd15e5b6c0eb2e99f0e98eadb48f5cd7
Louis Goerlt <louis.goerlt@gmail.com>
2026-09-25 15:37:08 +0200

    seven

--- klausur2/aufgabe7.pl
+++ klausur2/aufgabe7.pl
@@ -2,6 +2,12 @@
 
 raetsel(A, B, C) :-
   between(1, 9, A),
-  between(1, 9, B),
-  between(1, 9, C),
-  101 * C + 10 * A #= 11 * A + 11 * B.
+  between(1, 9, B), A =\= B,
+  between(1, 9, C), B =\= C, A =\= C,
+  101 * C + 10 * A =:= 11 * A + 11 * B.
+
+raetsel(A, B, C) :-
+  [A, B, C] ins 1..9,
+  all_different([A, B, C]),
+  101 * C + 10 * A #= 11 * A + 11 * B,
+  label([A, B, C]).
